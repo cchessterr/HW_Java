f57343c03dc756b48390276c3ef613c6db161ede
cchessterr <61469234+cchessterr@users.noreply.github.com>
2025-08-02 19:14:16 +0300

    Add files via upload

---
+++ HW01_Spring/SpringPenProbe/src/main/java/org/top/springpenprobe/StatusController.java
@@ -0,0 +1,18 @@
+package org.top.springpenprobe;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+    
+    @GetMapping
+    public String status() {
+        return "server is running";
+    }
+
+    @GetMapping("ping")
+    public String ping() {
+        return "pong";
+    }
+}
